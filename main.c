e86c68f7c902b796907af5bbf54c346ff9bd21ee
Carlos Garay <garaycarlosmauricio17@gmail.com>
2025-06-14 16:36:08 -0300

    Actualiza configuraciones de C y agrega archivo de encabezado main.h

--- main.c
+++ main.c
@@ -1,8 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 
-int main()
+// tpTopicos.exe indices_icc_general_capitulos.csv Indices_items_obra.csv
+
+int main(int argc, char *argv[])
 {
-    printf("Hello world!\n");
+    printf("Cantidad de argumentos: %d\n", argc);
+    for (int i = 0; i < argc; i++)
+    {
+        printf("argv[%d]: %s\n", i, argv[i]);
+    }
     return 0;
 }
---
+++ main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+#define ARG_INDICE_ICC 1
+#define ARG_INDICE_ITEMS 2
+#define TODO_OK 0
+#define ERR_ARCHIVO 1
+
+typedef struct {
+    char periodo[11]; // "aaaa-mm-dd"
+    char clasificador[20];// "Nivel general", "Capítulos", "Ítems"
+    char nivelGeneralAperturas[50];
+    char tipoVariable[20]; // "indice_icc", "var_mensual", "var_interanual"
+    double valor;
+} RegistroICC;
+
+
+#endif // MAIN_H_INCLUDED
