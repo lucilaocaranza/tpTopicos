844d68664d4e4a0eea8ae87e894ac80be6e45f07
Carlos Garay <garaycarlosmauricio17@gmail.com>
2025-06-14 18:01:58 -0300

    Se prepara programa para modificar a los archivos.

--- main.c
+++ main.c
@@ -1,13 +1,41 @@
 #include "main.h"
 
 // tpTopicos.exe indices_icc_general_capitulos.csv Indices_items_obra.csv
-
+/*typedef struct
+{
+    char periodo[11];      // "aaaa-mm-dd"
+    char clasificador[20]; // "Nivel general", "Cap�tulos", "�tems"
+    char nivelGeneralAperturas[50];
+    char tipoVariable[20]; // "indice_icc", "var_mensual", "var_interanual"
+    double valor;
+} RegistroICC;*/
 int main(int argc, char *argv[])
 {
-    printf("Cantidad de argumentos: %d\n", argc);
-    for (int i = 0; i < argc; i++)
+    RegistroICC registros_cap[MAX_REGISTROS];
+    int total = 0;
+
+    FILE *archCapitulos = fopen(argv[1], "r");
+    FILE *archItems = fopen(argv[2], "r");
+
+    if (!archCapitulos || !archItems)
     {
-        printf("argv[%d]: %s\n", i, argv[i]);
+        printf("Error abriendo archivos CSV.\n");
+        return ERR_ARCHIVO;
     }
-    return 0;
+
+    char registroData[256];
+    fgets(registroData, sizeof(registroData), archCapitulos); // Salto la cabecera
+    while (fgets(registroData, sizeof(registroData), archCapitulos))
+    {
+        char *periodo = strtok(registroData, ";\"");
+        char *nivel = strtok(NULL, ";\"");
+        char *indiceStr = strtok(NULL, ";\"\n");
+        // Se aplican las modificaciones a los campos.
+        // Se copia al array de registros.
+        total++;
+    }
+
+    fclose(archCapitulos);
+    fclose(archItems);
+    return TODO_OK;
 }
--- main.h
+++ main.h
@@ -7,14 +7,15 @@
 #define ARG_INDICE_ITEMS 2
 #define TODO_OK 0
 #define ERR_ARCHIVO 1
+#define MAX_REGISTROS 1000
 
-typedef struct {
-    char periodo[11]; // "aaaa-mm-dd"
-    char clasificador[20];// "Nivel general", "Capítulos", "Ítems"
+typedef struct
+{
+    char periodo[11];      // "aaaa-mm-dd"
+    char clasificador[20]; // "Nivel general", "Capï¿½tulos", "ï¿½tems"
     char nivelGeneralAperturas[50];
     char tipoVariable[20]; // "indice_icc", "var_mensual", "var_interanual"
     double valor;
 } RegistroICC;
-
 
 #endif // MAIN_H_INCLUDED
