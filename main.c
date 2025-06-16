12d9093430c7f76c64380328475e4e65e8a97841
FacuA0 <facundoamarilla28@gmail.com>
2025-06-16 02:22:26 -0300

    Funciones terminadas (sin probar, depende de TDAVector y TDAFecha)

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <math.h>
+
+#include "../../TDA/TDA2/TDAVector/Vector.h"
+#include "../../TDA/TDAFecha/Fecha.h"
 
 typedef struct {
     char periodo[11];
@@ -10,8 +14,18 @@ typedef struct {
     double var_mensual, var_interanual;
 } Fila;
 
+void calcularVarMensual(void* elem, void* datos);
+void calcularVarInteranual(void* elem, void* datos);
+int compararPorFecha(const void* a, const void* b);
+double calcularVarPorc(double indActual, double indPrevio);
+
 int main()
 {
+    Vector vec;
+    vectorCrear(&vec, sizeof(Fila));
+    vectorRecorrer(&vec, calcularVarMensual, &vec);
+    vectorRecorrer(&vec, calcularVarInteranual, &vec);
+
     printf("Hello world!\n");
     return 0;
 }
@@ -20,24 +34,60 @@ int main()
     Pasar NULL a filaPrev significa que es el primer mes y no debería ser calculado.
     La función que llame deberá comprobar si es una fecha válida.
 */
-void calcularVarMensual(Fila* filaAct, Fila* filaPrev) {
-    if (filaAnt == NULL) return;
+void calcularVarMensual(void* elem, void* datos) {
+    Fecha fechaAct, fechaPrev;
+    Fila* fila = elem, filaPrev = *fila;
+    Vector* vec = datos;
+    int res;
 
-    double porcentaje = calcularVarPorc(filaAct->var_mensual, filaPrev->var_mensual);
-
-    filaAct->var_mensual = floor(porcentaje * 100) / 100;
+    fechaDeCadena(&fechaAct, fila->periodo);
+    fechaPrev = fechaRestarMeses(&fechaAct, 1);
+    fechaACadena(&fechaPrev, filaPrev.periodo);
+    res = vectorOrdBuscar(vec, &filaPrev, compararPorFecha);
+    if (res != 0) {
+        double porcentaje = calcularVarPorc(fila->indice_icc, filaPrev.indice_icc);
+        fila->var_mensual = floor(porcentaje * 100) / 100;
+    }
 }
 
 /**
     Pasar NULL a filaPrev significa que no paso un año y no debería ser calculado.
     La función que llame deberá comprobar si es una fecha válida.
 */
-void calcularVarInteranual(Fila* filaAct, Fila* filaPrev) {
-    if (filaAnt == NULL) return;
+void calcularVarInteranual(void* elem, void* datos) {
+    Fecha fechaAct, fechaPrev;
+    Fila* fila = elem, filaPrev = *fila;
+    Vector* vec = datos;
+    int res;
 
-    double porcentaje = calcularVarPorc(filaAct->var_mensual, filaPrev->var_mensual);
+    fechaDeCadena(&fechaAct, fila->periodo);
+    fechaPrev = fechaRestarMeses(&fechaAct, 12);
+    fechaACadena(&fechaPrev, filaPrev.periodo);
+    res = vectorOrdBuscar(vec, &filaPrev, compararPorFecha);
+    if (res != 0) {
+        double porcentaje = calcularVarPorc(fila->indice_icc, filaPrev.indice_icc);
+        fila->var_mensual = floor(porcentaje * 100) / 100;
+    }
+}
 
-    filaAct->var_mensual = floor(porcentaje * 100) / 100;
+int compararPorFecha(const void* a, const void* b) {
+    const Fila* filaA = a;
+    const Fila* filaB = b;
+
+    Fecha fAct, fPrev;
+    fechaDeCadena(&fAct, filaA->periodo);
+    fechaDeCadena(&fPrev, filaB->periodo);
+
+    int cmp = fechaComparar(&fAct, &fPrev);
+    if (cmp != 0) {
+        return cmp;
+    }
+
+    if ((cmp = strcmp(filaA->Clasificador, filaB->Clasificador)) != 0) {
+        return cmp;
+    }
+
+    return strcmp(filaA->nivel_general_aperturas, filaB->nivel_general_aperturas);
 }
 
 double calcularVarPorc(double indActual, double indPrevio) {
